3186e9fd3405d6da79a57cdd7ee62e47acba2ba4
gatapro901-ops <gatapro901@gmail.com>
2026-03-17 10:10:57 +0300

    Sarah Architect: Building app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirm-password');
+const loginButton = document.querySelector('.login-button');
+
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    // تسجيل الدخول
+});
